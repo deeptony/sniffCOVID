8ac2e309f25fa543f4b8c6f4ddbabccd59ed7f57
Antonio-Linares <antonio@evolvia.co>
2020-04-04 17:40:09 +0200

    Tring again

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ def render_index():
 #package has key1, key2, key3, key4
 @server.route('/data', methods = ["POST"])
 def store_data():
-    body = request.data
+    body = request.get_json()
     print(body)
     return body
 
