b295b76831e6635e846bc6e4ed8ff64d20ededf0
Antonio-Linares <antonio@evolvia.co>
2020-04-04 16:14:01 +0200

    Data correctly reaches server

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 #performing relevant imports
-from flask import Flask, render_template, request, abort, session, flash, redirect
-
+from flask import Flask, render_template, request, abort, session, flash, redirect, jsonify
+from flask_cors import CORS
 #instantiating server
 server = Flask("flask_server")
+CORS(server)
 
 #defining routes
 
@@ -10,6 +11,14 @@ server = Flask("flask_server")
 @server.route('/')
 def render_index():
     return render_template('landing.html')
+
+#route for receiving data package
+#package has key1, key2, key3, key4
+@server.route('/data', methods = ["POST"])
+def store_data():
+    body = request.data
+    print(body)
+    return body
 
 
 #statement to initiate server
